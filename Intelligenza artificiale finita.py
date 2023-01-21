1f3d934b3fc5ff8fe3afc224d1b7ea46ecbbd3bf
Marco <121761685+mlinmg@users.noreply.github.com>
2023-01-21 21:40:21 +0100

    Update Intelligenza artificiale finita.py

--- Intelligenza artificiale finita.py
+++ Intelligenza artificiale finita.py
@@ -10,11 +10,8 @@ import tensorflow as tf
 from keras import layers
 import seaborn as sns
 from keras import regularizers
-# Caricamento del dataset
 
-load_model()
-data = pd.read_csv("dataset.csv")
-# Variabili
+# DNN Variables
 num_epochs = 100
 batch_size = 32
 l1_reg = 0.1
@@ -25,17 +22,16 @@ num_neurons3 = 256
 num_neurons4 = 128
 num_neurons5 = 1
 
-# Selezione delle colonne da utilizzare come input e output
-X = data.drop(columns=["target", "Indirizzo","Unnamed: 0"])
+#Splitting dataset
+X = data.drop(columns=["target", "Indirizzo", "Unnamed: 0"])
 y = data["target"]
 
 
-# Utilizzo della codifica Label Encoding per convertire le colonne con stringhe in valori numerici
+# Creating aclass for converting string into numeric values
 class MultiColumnLabelEncoder:
 
     def __init__(self, columns=None):
-        self.columns = columns # array of column names to encode
-
+        self.columns = columns  # array of column names to encode
 
     def fit(self, X, y=None):
         self.encoders = {}
@@ -44,7 +40,6 @@ class MultiColumnLabelEncoder:
             self.encoders[col] = LabelEncoder().fit(X[col])
         return self
 
-
     def transform(self, X):
         output = X.copy()
         columns = X.columns if self.columns is None else self.columns
@@ -52,10 +47,8 @@ class MultiColumnLabelEncoder:
             output[col] = self.encoders[col].transform(X[col])
         return output
 
-
     def fit_transform(self, X, y=None):
-        return self.fit(X,y).transform(X)
-
+        return self.fit(X, y).transform(X)
 
     def inverse_transform(self, X):
         output = X.copy()
@@ -64,42 +57,49 @@ class MultiColumnLabelEncoder:
             output[col] = self.encoders[col].inverse_transform(X[col])
         return output
 
-multi = MultiColumnLabelEncoder(columns=['Contratto', 'Tipologia', 'Piano', 'Stato', 'Climatizzazione', 'City',"Riscaldamento"])
+
+multi = MultiColumnLabelEncoder(
+    columns=['Contratto', 'Tipologia', 'Piano', 'Stato', 'Climatizzazione', 'City', "Riscaldamento"])
 X = multi.fit_transform(X)
 
 # Divisione del dataset in training set e test set
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+# Dataset creation
+try:
+    model = load_model("deep_learning_model.h5")
+except:
 
-# Creazione del modello di deep learning
-model = keras.Sequential()
+    data = pd.read_csv("dataset.csv")
 
-# Aggiunta della prima hidden layer con L1 regularization
-model.add(layers.Dense(num_neurons1, input_shape=(X_train.shape[1],), kernel_regularizer=regularizers.l1(l1_reg)))
-model.add(layers.Activation('relu'))
-model.add(layers.Dropout(0.5))
+    # Model creation
+    model = keras.Sequential()
 
-# Aggiunta della seconda hidden layer con L2 regularization
-model.add(layers.Dense(num_neurons2, kernel_regularizer=regularizers.l1(l1_reg)))
-model.add(layers.Activation('relu'))
-model.add(layers.Dropout(0.5))
+    # Aggiunta of layer con regularization and dropout
+    model.add(layers.Dense(num_neurons1, input_shape=(X_train.shape[1],), kernel_regularizer=regularizers.l1(l1_reg)))
+    model.add(layers.Activation('relu'))
+    model.add(layers.Dropout(0.5))
 
-model.add(layers.Dense(num_neurons3, kernel_regularizer=regularizers.l1(l1_reg)))
-model.add(layers.Activation('relu'))
-model.add(layers.Dropout(0.5))
+    model.add(layers.Dense(num_neurons2, kernel_regularizer=regularizers.l1(l1_reg)))
+    model.add(layers.Activation('relu'))
+    model.add(layers.Dropout(0.5))
+    
+    model.add(layers.Dense(num_neurons3, kernel_regularizer=regularizers.l1(l1_reg)))
+    model.add(layers.Activation('relu'))
+    model.add(layers.Dropout(0.5))
 
-model.add(layers.Dense(num_neurons4, kernel_regularizer=regularizers.l1(l1_reg)))
-model.add(layers.Activation('relu'))
-model.add(layers.Dropout(0.5))
+    model.add(layers.Dense(num_neurons4, kernel_regularizer=regularizers.l1(l1_reg)))
+    model.add(layers.Activation('relu'))
+    model.add(layers.Dropout(0.5))
 
 
-# Aggiunta della output layer
-model.add(layers.Dense(num_neurons5))
-model.add(layers.Activation('linear'))
+    # Aggiunta del output layer
+    model.add(layers.Dense(num_neurons5))
+    model.add(layers.Activation('linear'))
 
-# Compilazione del modello utilizzando l'ottimizzatore Adam
-model.compile(loss='mean_absolute_error', optimizer='adam')
+    # Compilazione del modello utilizzando l'ottimizzatore Adam
+    model.compile(loss='mean_absolute_error', optimizer='adam')
 
-# Addestramento del modello
+    # Addestramento del modello
 history = model.fit(X_train, y_train, epochs=num_epochs, batch_size=batch_size, validation_data=(X_test, y_test))
 
 # Valutazione del modello sui dati di test
@@ -111,9 +111,6 @@ print("Mean Absolute Error on test set:", test_loss)
 y_pred = model.predict(X_test)
 # Stampa del valore della mean absolute error tra le previsioni e i valori reali
 print("Mean Absolute Error:", mean_absolute_error(y_test, y_pred))
-
-#Grafico della curva di apprendimento
-# Addestramento del modello
 
 # Recupero dei valori di perdita e accuratezza per ogni epoca
 loss = history.history['loss']
@@ -132,13 +129,8 @@ plt.ylabel("Perdita")
 plt.title("Epoche: {}, Batch: {}, L1: {}, L2: {}, Neuroni: input: {}, hidden#1: {}, hidden#2: {}, hidden#3: {}, output: {}".format(num_epochs, batch_size, l1_reg, l2_reg, num_neurons1, num_neurons2, num_neurons3, num_neurons4, num_neurons5))
 plt.legend()
 
-
-# Aggiunta della descrizione
-
 # Salva il modello
 model.save("deep_learning_model.h5")
 
 # Grafico della curva di apprendimento
-
-
 plt.show()
